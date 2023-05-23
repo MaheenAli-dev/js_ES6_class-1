96474fb9efef8f4d191401de695a2fb28990f0af
Maheen_Ali_dev <125376391+MaheenAli-dev@users.noreply.github.com>
2023-05-23 14:39:35 +0500

    Add files via upload

---
+++ js es6/java.js
@@ -0,0 +1,38 @@
+// const myfunc = () => {
+//     console.log("hello");
+
+//     const anotherfunc = () => {
+//         console.log("hi");
+
+//     }
+//     anotherfunc();
+// }
+
+
+
+// myfunc();
+
+// setTimeout(myfunc, 3000);
+
+// setTimeout(() => {
+//     console.log("hello");
+// }, 3000);
+
+
+
+// const myobj = {
+//     username: "maheen",
+//     id: "000000"
+
+// }
+// console.log("my name is " + myobj.username);
+
+// const  { username, id } = myobj;
+// console.log(`My Name is ${username} and my Uni ID is
+
+
+
+// 8888888`);
+
+
+
